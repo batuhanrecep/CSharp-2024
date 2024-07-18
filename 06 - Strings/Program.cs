16cac0e72dfae403096cd97e46d7fdcb03f8b544
byrchard <batuhanrecep06@hotmail.com>
2024-07-18 18:51:24 +0300

    Strings finished

--- 06 - Strings/Program.cs
+++ 06 - Strings/Program.cs
@@ -11,7 +11,39 @@ namespace _06___Strings
         static void Main(string[] args)
         {
             //Intro();
+            string sentence = "My name is Batuhan Recep";
 
+            var result1 = sentence.Length;
+            var result2 = sentence.Clone();
+            sentence = "My name is Batuhan ";
+          
+            bool result3 = sentence.EndsWith("p");
+            bool result4 = sentence.StartsWith("My name");
+
+            var result5 = sentence.IndexOf("Namee");
+            var result6 = sentence.IndexOf(" ");
+            var result7 = sentence.LastIndexOf(" ");
+            var result8 = sentence.Insert(0, "Hello");
+            var result9 = sentence.Substring(3, 4);
+            var result10 = sentence.ToLower();
+            var result11 = sentence.ToUpper();
+            var result12 = sentence.Replace(" ", "-");
+            var result13 = sentence.Remove(2, 5);
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.WriteLine(result4);
+            Console.WriteLine(result5);
+            Console.WriteLine(result6);
+            Console.WriteLine(result7);
+            Console.WriteLine(result8);
+            Console.WriteLine(result9);
+            Console.WriteLine(result10);
+            Console.WriteLine(result11);
+            Console.WriteLine(result12);
+            Console.WriteLine(result13);
+            Console.ReadLine();
 
             Console.ReadLine();
         }
