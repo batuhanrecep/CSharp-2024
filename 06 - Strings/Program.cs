9357aef4c379ecbb53dc0ba8dab228cf72daf168
byrchard <batuhanrecep06@hotmail.com>
2024-07-18 18:41:50 +0300

    Strings added

---
+++ 06 - Strings/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06___Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intro();
+
+
+            Console.ReadLine();
+        }
+
+        private static void Intro()
+        {
+            string city = "Ankara";
+            string city2 = "İstanbul";
+
+
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+
+            //1 string result = city + city2;
+            //Console.WriteLine(result);
+
+            //2
+            Console.WriteLine(String.Format(("{0}{1}"),city,city2));
+        }
+    }
+}
