3b6ee700103c5e0961392399a3cc79fff132683a
byrchard <batuhanrecep06@hotmail.com>
2024-07-18 19:15:25 +0300

    .

---
+++ 07 - Classes/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07___Classes
+{
+    internal class Customer
+    {
+        public int Id { get; set; }
+
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+
+
+        public string FirstName { get; set; }
+
+        //OLD WAY
+
+        //private string _firstname;
+
+
+        //public string FirstName {
+        //    get
+        //    {
+        //        return "Mr."+_firstname; 
+
+        //    }
+        //    set
+        //    {
+        //        _firstname = value;
+        //    }
+        //}
+    }
+}
---
+++ 07 - Classes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07___Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+
+            Customer customer = new Customer(); //resharper can easily transform to next one
+
+            customer.City = "Ankara";
+            customer.FirstName = "Batuhan";
+            customer.LastName = "Recep";
+            customer.Id = 1;
+
+
+            Customer customer2 = new Customer
+            {
+                City = "Ankara",
+                FirstName = "Batuhan2",
+                Id = 1,
+                LastName = "Recep2"
+            };
+
+
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+
+        }
+    }
+
+
+
+
+}
