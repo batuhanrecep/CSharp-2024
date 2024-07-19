d3a52e7b90c34a8a253c02220a40939a7dc75c22
byrchard <batuhanrecep06@hotmail.com>
2024-07-19 20:19:29 +0300

    .

---
+++ 14 - Demo3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14___Demo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new DatabaseLogger();
+            customerManager.Add();
+
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        //Property Injection. Usually, constructors will be use for that
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+
+            Logger.Log();
+            Console.WriteLine("Customer added!");
+            
+
+        }
+    }
+    interface ILogger
+    {
+        void Log();
+    }
+
+
+    class DatabaseLogger: ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database!");
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file!");
+        }
+    }
+
+    class SmsLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Sms!");
+        }
+    }
+}
