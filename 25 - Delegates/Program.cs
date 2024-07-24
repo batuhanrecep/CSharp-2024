3a7a76d7753768ac9852292ad30bfb9165c7da36
byrchard <batuhanrecep06@hotmail.com>
2024-07-24 23:57:08 +0300

    Delegates tested by different types

--- 25 - Delegates/Program.cs
+++ 25 - Delegates/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace _25___Delegates
 {
-    public delegate void MyDelegate(); 
+    public delegate void MyDelegate();  
+    public delegate void MyDelegate2(string text);
+    public delegate int MyDelegate3(int number1, int number2);
     class Program
     {
         static void Main(string[] args)
@@ -16,7 +18,26 @@ namespace _25___Delegates
             customerManager.SendMessage();
             customerManager.ShowAlert();
 
-            MyDelegate myDeletgate = customerManager.SendMessage;
+
+            MyDelegate myDelegate = customerManager.SendMessage;
+            myDelegate += customerManager.ShowAlert;
+            myDelegate -= customerManager.ShowAlert;
+            
+            MyDelegate2 myDelegate2 = customerManager.SendMessage2;
+            myDelegate2 += customerManager.ShowAlert2;
+
+            Maths maths = new Maths();
+
+            MyDelegate3 myDelegate3 = maths.Sum;
+            myDelegate3 += maths.Multiply;  //When the delegate have return type, it will run the last sample
+
+            myDelegate2("Test");
+            myDelegate();
+            var result = myDelegate3(5,6);
+
+            Console.WriteLine(result);
+            
+            
             Console.ReadLine();
         }
 
@@ -32,6 +53,29 @@ namespace _25___Delegates
         public void ShowAlert()
         {
             Console.WriteLine("Be Careful!");
+        }
+        public void SendMessage2(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void ShowAlert2(string alert)
+        {
+            Console.WriteLine(alert);
+        }
+
+    }
+
+    public class Maths
+    {
+        public int Sum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
         }
     }
 
