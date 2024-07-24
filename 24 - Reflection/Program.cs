3dc492702263d94cfec2903f8ae0e934b45a4656
byrchard <batuhanrecep06@hotmail.com>
2024-07-24 23:38:51 +0300

    Reflection Tutorial Ended

--- 24 - Reflection/Program.cs
+++ 24 - Reflection/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,9 +19,38 @@ namespace _24___Reflection
 
             //Reflection should not use for everything. Its an expensive object. Reflection should use when it needed
             var type = typeof(MathOperations);
-            MathOperations mathOperations =(MathOperations)Activator.CreateInstance(type,6,7);
-            Console.WriteLine(mathOperations.Sum2());
-            Console.WriteLine(mathOperations.Sum(11,12));
+            //First example, CreateInstance Method
+
+            //MathOperations mathOperations =(MathOperations)Activator.CreateInstance(type,6,7);
+            //Console.WriteLine(mathOperations.Sum2());
+            //Console.WriteLine(mathOperations.Sum(11,12));
+
+            //Second Example
+            //With GetMethod, we can reach the method(Sum2) and we can invoke it with or without parameters
+            var instance = Activator.CreateInstance(type, 6, 5);
+            Console.WriteLine(instance.GetType().GetMethod("Sum2").Invoke(instance,null));
+
+
+            MethodInfo methodInfo = instance.GetType().GetMethod("Sum2");
+            Console.WriteLine(methodInfo.Invoke(instance, null));
+
+
+            //With reflection, we can reach the features of an object's (Methods, attributes etc.)
+            var methods = type.GetMethods();
+
+            foreach (var info in methods)
+            {
+                Console.WriteLine("Name of method : {0}",info.Name);
+                foreach (var parameterInfo in info.GetParameters())
+                {
+                    Console.WriteLine("Parameters : {0}", parameterInfo.Name);
+                }
+                foreach (var attribute in info.GetCustomAttributes())
+                {
+                    Console.WriteLine("Attribute : {0}", attribute.GetType().Name);
+                }
+            }
+
             Console.ReadLine();
         }
     }
@@ -51,11 +82,20 @@ namespace _24___Reflection
             return _number1 + _number2;
         }
 
+        [MethodName("Don't Multiply")]
         public int Multiply2()
         {
             return _number1 * _number2;
         }
 
+    }
+
+    public class MethodNameAttribute : Attribute
+    {
+        public MethodNameAttribute(string name)
+        {
+            
+        }
     }
 
 }
