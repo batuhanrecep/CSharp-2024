b9c66c26e6a7960d4af0e9730f81bddf494fc9c1
byrchard <batuhanrecep06@hotmail.com>
2024-07-24 23:43:33 +0300

    Delegate Project has been created

---
+++ 25 - Delegates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25___Delegates
+{
+    public delegate void MyDelegate(); 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.SendMessage();
+            customerManager.ShowAlert();
+
+            MyDelegate myDeletgate = customerManager.SendMessage;
+            Console.ReadLine();
+        }
+
+    }
+
+    public class CustomerManager
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        public void ShowAlert()
+        {
+            Console.WriteLine("Be Careful!");
+        }
+    }
+
+}
