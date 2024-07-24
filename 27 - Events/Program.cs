19e1c64cca83ec7c9f8eae704fefa3931e64f423
byrchard <batuhanrecep06@hotmail.com>
2024-07-25 00:36:23 +0300

    Events project added

---
+++ 27 - Events/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27___Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public delegate void StockControl();
+    public class Product
+    {
+        private int _stock;
+
+        public Product(int stock)
+        {
+            _stock = stock;
+        }
+
+        public event StockControl StockControlEvent;
+        public string ProductName { get; set; }
+        public int Stock { get; set; }
+
+        public void Sell(int amount)
+        {
+            Stock -= amount;
+        }
+    }
+}
